2282462afae2b3912aa04f9dd6942d8c72427e80
sheepfish5 <yuyangbusiness@foxmail.com>
2024-03-10 16:39:18 +0800

    add basic words

---
+++ word_lib.h
@@ -0,0 +1,38 @@
+
+#ifndef WORD_LIB_H
+#define WORD_LIB_H
+
+#define FOUR_WORD_LEN 128
+#define THREE_WORD_LEN 96
+
+/* "下一站  " length = 128B */
+extern unsigned char xyz[];
+
+/* "  到了" length=96B */
+extern unsigned char dl[];
+
+/* "火车站" length=96B */
+extern unsigned char hcz[];
+
+/* "文海酒店" length=128B */
+extern unsigned char whjd[];
+
+/* "张家坊" length=96B */
+extern unsigned char zjf[];
+
+/* "赣江大桥" length=128B */
+extern unsigned char gjdq[];
+
+/* "岭上村" length=96B */
+extern unsigned char lsc[];
+
+/* "天际光电" length=128B */
+extern unsigned char tjgd[];
+
+/* "中国石化" length=128B */
+extern unsigned char zgsh[];
+
+/* "仁安医院" length=128B */
+extern unsigned char rayy[];
+
+#endif
